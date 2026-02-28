f9b70d98148b477ed967f7d80477270815055c47
Muhammad Zakir <hi@zakir.id>
2026-03-01 00:12:47 +0700

    Update host to prevent network issue with Docker

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from application import create_app
 application = create_app()
 
 if __name__ == "__main__":
-    application.run(debug=True, port=5000)
+    application.run(host="0.0.0.0", debug=True, port=5000)
